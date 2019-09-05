0024b621437947334c0b2d5632e9333f6f78cfed
Kevin Wang <kevinwang300@berkeley.edu>
2019-09-04 18:42:15 -0700

    Updates

---
+++ precache-manifest.b6315a22b91a19ba3e43d608fe0669eb.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bb5b0666ae196bbb903529ede0b618",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "32f4bb6046364bb2dbfd",
+    "url": "/personal-website/static/css/2.1e871fd3.chunk.css"
+  },
+  {
+    "revision": "4763713a4dcca5c59d24",
+    "url": "/personal-website/static/css/main.491cc519.chunk.css"
+  },
+  {
+    "revision": "32f4bb6046364bb2dbfd",
+    "url": "/personal-website/static/js/2.ada6ea83.chunk.js"
+  },
+  {
+    "revision": "4763713a4dcca5c59d24",
+    "url": "/personal-website/static/js/main.ae4ab6fd.chunk.js"
+  },
+  {
+    "revision": "5975e6ab0606452d2345",
+    "url": "/personal-website/static/js/runtime~main.44c44f6c.js"
+  },
+  {
+    "revision": "43b72e0af3b72d6080a5d39bb31a3d3a",
+    "url": "/personal-website/static/media/AiRPicture.43b72e0a.jpg"
+  },
+  {
+    "revision": "8be2fa01c9e9c0e6c5b338b5be8aa79e",
+    "url": "/personal-website/static/media/CodeologyPicture.8be2fa01.jpg"
+  },
+  {
+    "revision": "621cd581f40a650427fdf7808ecf78b3",
+    "url": "/personal-website/static/media/Kevin.Wang.621cd581.pdf"
+  },
+  {
+    "revision": "2612c3e8818001db8a634397b06cbc55",
+    "url": "/personal-website/static/media/TKEPicture.2612c3e8.jpg"
+  },
+  {
+    "revision": "8d489090de2438249f63242e843e8411",
+    "url": "/personal-website/static/media/UCBerkeleyBackground.8d489090.jpg"
+  },
+  {
+    "revision": "a663a6e51a27c3a3669eae5e1e5d20d9",
+    "url": "/personal-website/static/media/able2shineLogo.a663a6e5.jpg"
+  },
+  {
+    "revision": "2d71ad5209dff224f8d31557416aee0c",
+    "url": "/personal-website/static/media/audioflexicaScreenshot.2d71ad52.png"
+  },
+  {
+    "revision": "ba5c12e436cc6be1d117676316c09394",
+    "url": "/personal-website/static/media/coffeeChat.ba5c12e4.jpg"
+  },
+  {
+    "revision": "97a7cd2640dffde85639a47af63c54d0",
+    "url": "/personal-website/static/media/datastore.97a7cd26.jpg"
+  },
+  {
+    "revision": "3a5b877c79d887fb32ef5615d7374247",
+    "url": "/personal-website/static/media/homebuddyScreenshot.3a5b877c.png"
+  },
+  {
+    "revision": "d1a3d596632509db1f67c30e677871a1",
+    "url": "/personal-website/static/media/kevin.d1a3d596.JPEG"
+  },
+  {
+    "revision": "e1b135880fda608e99ec1dc790e6920c",
+    "url": "/personal-website/static/media/laptopBackground.e1b13588.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3e89a9e4d9b8b215def9309111518709",
+    "url": "/personal-website/static/media/sabreLogo.3e89a9e4.svg"
+  },
+  {
+    "revision": "11d2c1ad3e833e0241dc62bf0f26b7f1",
+    "url": "/personal-website/static/media/sabreOffice.11d2c1ad.jpg"
+  },
+  {
+    "revision": "aee464882746b12564567843dbe941ed",
+    "url": "/personal-website/static/media/sfSkyline.aee46488.jpg"
+  },
+  {
+    "revision": "d9ef40cc755515ab7e60745e69456aa1",
+    "url": "/personal-website/static/media/splunkOffice.d9ef40cc.jpg"
+  },
+  {
+    "revision": "5dfd47fef13aedd83a205a65fb5eac96",
+    "url": "/personal-website/static/media/teslaLogo.5dfd47fe.svg"
+  },
+  {
+    "revision": "cf921ffb953c9f967481dec413299596",
+    "url": "/personal-website/static/media/teslaOffice.cf921ffb.jpg"
+  },
+  {
+    "revision": "7f1c0fd075f29d1db1c11166e0e60b5b",
+    "url": "/personal-website/static/media/titanicSplitting.7f1c0fd0.jpg"
+  },
+  {
+    "revision": "f3c3612efac4811b5bd4ba89d1a196dd",
+    "url": "/personal-website/static/media/worldlyLogo.f3c3612e.png"
+  }
+]);
